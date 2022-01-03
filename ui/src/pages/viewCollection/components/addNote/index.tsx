f53286da27a017132df239a7164c1ba522c60253
FhatuL <iqmakesmusic@gmail.com>
2022-01-04 00:46:09 +0200

    added dark mode colors

--- ui/src/pages/viewCollection/components/addNote/index.tsx
+++ ui/src/pages/viewCollection/components/addNote/index.tsx
@@ -13,7 +13,10 @@ const AddNote = () => {
 					fill="#000000"
 				>
 					<path d="M0 0h24v24H0V0z" fill="none" />
-					<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+					<path
+						className={style.path}
+						d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"
+					/>
 				</svg>
 			</span>
 		</div>
