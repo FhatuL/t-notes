55c2f0869c4e846d22417ce4354fdee936f341ba
FhatuL <iqmakesmusic@gmail.com>
2022-01-08 09:29:00 +0200

    added some transitions

--- ui/src/components/nav/components/toggleDark/index.tsx
+++ ui/src/components/nav/components/toggleDark/index.tsx
@@ -1,14 +1,24 @@
 import style from "./toggle.module.scss";
+import "./transitions.css";
 import svg from "./svgs";
 import {useEffect, useState} from "react";
-
+import {SwitchTransition, CSSTransition} from "react-transition-group";
+import React from "react";
 const {Dark, Light} = svg;
+
+interface Props {
+	children: React.ReactChild | React.ReactChildren;
+}
 
 const ToggleDark = () => {
 	const [isDark, setIsDark] = useState<boolean>(false);
 
 	const toggleDarkMode = () => {
 		setIsDark(!isDark);
+	};
+
+	const ToggleComp: React.FC<Props> = ({children}) => {
+		return <>{children}</>;
 	};
 
 	useEffect(() => {
@@ -22,7 +32,17 @@ const ToggleDark = () => {
 	return (
 		<div className={style.parent} onClick={toggleDarkMode}>
 			<div className={[style.item].join(" ")} title="toggle dark mode">
-				{!isDark ? <Dark /> : <Light />}
+				<SwitchTransition mode="out-in">
+					<CSSTransition
+						key={isDark ? "dark" : "light"}
+						classNames="fade"
+						addEndListener={(node, done) =>
+							node.addEventListener("transitionend", done, false)
+						}
+					>
+						<ToggleComp>{isDark ? <Light /> : <Dark />}</ToggleComp>
+					</CSSTransition>
+				</SwitchTransition>
 			</div>
 		</div>
 	);
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -6,11 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<HashRouter>
-			<App />
-		</HashRouter>
-	</React.StrictMode>,
+	// <React.StrictMode>
+	<HashRouter>
+		<App />
+	</HashRouter>,
+	// </React.StrictMode>,
 	document.getElementById("root")
 );
 
