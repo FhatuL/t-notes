e0061551267aa7396c6cfaa974aac78e54e850a2
FhatuL <iqmakesmusic@gmail.com>
2022-01-24 16:31:03 +0200

    minor commit

--- api/src/middleware/cors.ts
+++ api/src/middleware/cors.ts
@@ -1,7 +1,11 @@
 import CORS from "cors";
 
 const options: CORS.CorsOptions = {
-	origin: ["http://localho.st:3000", "https://fhatul.github.io"],
+	origin: [
+		"http://localho.st:3000",
+		"http://localhost:3000",
+		"https://fhatul.github.io",
+	],
 };
 
 const cors = CORS(options);
